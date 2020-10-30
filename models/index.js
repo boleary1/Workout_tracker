1c659dcc1da44532e2751124a483a3bb8f04fbd0
Brian OLeary <oleary.brian1@gmail.com>
2020-10-30 16:07:49 -0400

    spelling

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 //thes models could probably be combined into one, but I left them seperate incase I want to add users or some other funcionality eventually.
 module.exports = {
-	Workout: require('./Workout')
+	Workout: require('./workout')
 };
